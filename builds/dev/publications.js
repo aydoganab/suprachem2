867c22d9a2e1dd8aab4db9430c8957bfc324004c
A. Aydogan Office <aaydogan@gmail.com>
2021-10-31 17:35:01 +0300

    new members and publications.js update (show hide abstract via js)

--- builds/dev/publications.js
+++ builds/dev/publications.js
@@ -48,21 +48,53 @@ xhr.onload = function () {
         }
 
         if (s.abstract != "") {
+            let show_abstract = document.createElement("a");
+            show_abstract.setAttribute("href", "#");
+            show_abstract.setAttribute("data-a", s.num);
+            show_abstract.classList.add("card-link", "small", "show-abstract");
+            show_abstract.appendChild(document.createTextNode("Show abstract"));
+            card_body.appendChild(show_abstract);
             let p_abstract = document.createElement('p');
-            p_abstract.classList.add('card-text', 'd-none', 'd-sm-block');
+            p_abstract.setAttribute("id", "abstract-" + s.num);
+            p_abstract.classList.add('card-text', 'd-none', "abstract");
             p_abstract.appendChild(document.createTextNode(s.abstract));
             card_body.appendChild(p_abstract);
         }
 
-        let doi = document.createElement('a');
-        doi.href = "https://doi.org/" + s.doi;
-        doi.target = "_blank";
-        doi.className = "card-link";
-        doi.innerHTML = '<svg style="vertical-align: 0em" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/></svg> DOI: ' + s.doi;
-        card_body.appendChild(doi);
+        if (s.doi !== "") {
+            let card_footer = document.createElement("div");
+            card_footer.classList.add("card-footer", "text-end");
+            let doi = document.createElement('a');
+            doi.href = "https://doi.org/" + s.doi;
+            doi.target = "_blank";
+            doi.classList.add('card-link', "text-truncate");
+            doi.innerHTML = '<svg style="vertical-align: 0em" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/><path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/></svg> DOI: ' + s.doi;
+            card_footer.appendChild(doi);
+            card.appendChild(card_footer);
+        }
+
 
 
         document.getElementById('pubs').appendChild(card);
+    }
+
+    //show abstracts
+    let show_abstracts = document.getElementsByClassName("show-abstract");
+    for (let i = 0; i < show_abstracts.length; i++) {
+        show_abstracts[i].addEventListener("click", function (e) {
+            e.preventDefault();
+            let a = this.getAttribute("data-a");
+            let p_abstract = document.getElementById("abstract-" + a);
+            if (this.classList.contains("show")){
+                p_abstract.classList.add("d-none");
+                this.classList.remove("show");
+                this.childNodes[0].textContent = "Show abstract";
+            }else {
+                p_abstract.classList.remove("d-none");
+                this.classList.add("show");
+                this.childNodes[0].textContent = "Hide abstract";
+            }
+        });
     }
 };
 xhr.open('GET', 'publications.json', true);
