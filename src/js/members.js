a25f0210876532ea48cad9242ea091cf064749cc
A.Aydogan Huawei Laptop 1 <aaydogan@gmail.com>
2025-04-22 00:06:30 +0300

    json data loads now passes cache busting string

--- src/js/members.js
+++ src/js/members.js
@@ -51,5 +51,13 @@ xhr.onload = function () {
 
     }
 };
-xhr.open('GET', 'members.json', true);
+const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}${month}${day}`;
+};
+const currentDate = new Date();
+xhr.open('GET', 'members.json?_'+formatDate(currentDate), true);
 xhr.send();
--- src/js/research.js
+++ src/js/research.js
@@ -38,5 +38,13 @@ xhr.onload = function () {
 
     }
 };
-xhr.open('GET', 'publications.json', true);
+const formatDate = (date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}${month}${day}`;
+};
+const currentDate = new Date();
+xhr.open('GET', 'publications.json?_'+formatDate(currentDate), true);
 xhr.send();
